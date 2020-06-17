fa5840cd771ca1d217ffffc23394bbe512ab8d4e
mingyuchoo <mingyuchoo@gmail.com>
2020-06-17 22:19:55 +0900

    modified

--- 20-express-typescript/src/index.ts
+++ 20-express-typescript/src/index.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
+const port = 3000;
 
-app.get('/', (request: Request, response: Response, next: NextFunction) => {
-  response.send('hello');
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('hello');
 });
 
-app.listen(3000, () => {
-  console.log('start');
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
