0802d3510ff02fb83275fff867b9281b940c11c9
mingyuchoo <mingyuchoo@gmail.com>
2020-07-11 18:49:10 +0900

    modified

--- 20-express-typescript/src/index.ts
+++ 20-express-typescript/src/index.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
 
 const app = express();
+const host = '0.0.0.0';
 const port = 3000;
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Example app listening at ${host}:${port}`);
 });
--- 30-express-typescript-with-sequelize-WIP/README.md
+++
@@ -1,2 +0,0 @@
-ref: <https://helloinyong.tistory.com/124>
-ref: <https://kimsejune.github.io/2019/02/27/ts-connect-sequelize/>
